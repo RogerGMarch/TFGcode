241575757ed344ed4a84874a9e1931198b9e432a
RogerGMarch <37028578+RogerGMarch@users.noreply.github.com>
2021-12-24 18:02:21 +0100

    24 Dec
    Es fa un codi per obtenir la llista dels noms dels TTres sense nons duplicats ni el info TTree.

--- 24Dec/ReadRawNames.C
+++ 24Dec/ReadRawNames.C
@@ -1,20 +1,52 @@
 // Test to see if we get the raw names right
 // remember to check the library path
 
+// There  is a problem, we have duplicated RawNames, we introduce a modification to only get the second one string.
 
-R__LOAD_LIBRARY(/home/roger/Desktop/23Dec/3He_DSP_routines.C+);
 
+R__LOAD_LIBRARY(/media/roger/28721BB8721B8A28/root/TFGcode-main/24Dec/3He_DSP_routines.C+);
 
+#include <typeinfo>
 void RawNames(string FileName=""){
 
-    for (auto&& keyAsObj : file->GetListOfKeys()){
 
+    
+
+    TFile *f = new TFile(FileName.c_str());
+
+    std:: vector<const char*> Names;
+
+
+    for (auto&& keyAsObj : *f->GetListOfKeys()){
 
         auto key = (TKey*) keyAsObj;
+
+        Names.push_back(key->GetName());
+
         cout << key->GetName() << " " << key->GetClassName() << endl;
+        cout << typeid(key->GetName()).name() << endl;
+
+    }
 
 
-}
+// remove info TTre name
+    Names.pop_back();
+// Remove duplicate names (root files have a duplicate name tree for the metadata)
+// primera iteració borra raw_0 0 per cada iteració es van reduint en 1 la posicio del index
+
+    for(int i=0; i<Names.size();i++){
+
+            Names.erase(Names.begin()+i);
+
+        }
+
+
+    for (const char* i: Names)
+
+        std::cout << i << ' ';
+    
+
+
 
 
 
